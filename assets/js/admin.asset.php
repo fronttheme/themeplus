9880c711a9507f2668eec28a97f994ffac60a558
FARUK AHMED <farukahmedinbox@gmail.com>
2026-03-26 00:45:26 +0600

    Update: package.json

--- assets/js/admin.asset.php
+++ assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '687867d4463d5dbebc4c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '934869a0d3d102c60033');
